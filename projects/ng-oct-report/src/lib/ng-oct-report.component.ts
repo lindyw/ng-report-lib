07d1b9366ca1ede81bd2118b2c605ef399a5e07f
Woon Him Wong <5020925-woonhwong@users.noreply.gitlab.com>
2022-08-06 14:00:11 +0100

    showcase test lib with dynamic input header's data

--- projects/ng-oct-report/src/lib/ng-oct-report.component.ts
+++ projects/ng-oct-report/src/lib/ng-oct-report.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { merge, Observable, Subject } from 'rxjs';
 import { Header, TopAlert } from './ng-oct-report.interface';
 import { NgOctReportService } from './ng-oct-report.service';
 
 @Component({
-    selector: 'lib-ng-oct-report',
+    selector: 'ng-oct-report',
     templateUrl: './ng-oct-report.component.html',
-    styleUrls: ['./ng-oct-report.component.css']
+    styleUrls: ['./ng-oct-report.component.scss']
 })
 export class NgOctReportComponent implements OnInit {
+
+    @Input() header: Header | null = null;
+    @Input() topAlerts: TopAlert[] | null = null;
 
     header$ = new Subject<Header>();
     topAlerts$ = new Subject<TopAlert[]>();
--- projects/ng-report-showcase/src/app/app.component.ts
+++ projects/ng-report-showcase/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TopAlert } from 'projects/ng-oct-report/src/lib/ng-oct-report.interface';
+import { NgOctReportService } from 'projects/ng-oct-report/src/public-api';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'ng-report-showcase';
+
+    header = {
+        date: {
+            start: '2022-08-01',
+            end: '2022-08-06'
+        },
+        tenant: 'Octiga Software Ltd.'
+    }
+
+    topAlerts: TopAlert[] =
+        [
+            { severity: 'critical', timestamp: '2022-08-02T22:28:999Z', user: 'bob.husky@octiga.com', 'description': 'Login from Country not in Allowed List' }
+        ]
+
+
+    constructor(private reportService: NgOctReportService) {
+
+    }
+
+    ngOnInit(): void {
+
+    }
+
+
 }
