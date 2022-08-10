f2a3161f436d9553ce23b9f6f518703273654e1d
Woon Him Wong <5020925-woonhwong@users.noreply.gitlab.com>
2022-08-10 01:06:45 +0100

    build with ngPackagr and copy assets

--- projects/ng-oct-report/src/public-api.ts
+++ projects/ng-oct-report/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/ng-oct-report.service';
 export * from './lib/ng-oct-report.component';
 export * from './lib/ng-oct-report.module';
+export * from './lib/ng-oct-report.interface'
