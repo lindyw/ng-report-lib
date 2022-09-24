a22c83af3b82c1df178f190ead6e076c408a6c93
Woon Him Wong <5020925-woonhwong@users.noreply.gitlab.com>
2022-09-24 04:20:34 +0100

    export services/category.service

--- projects/ng-oct-report/src/public-api.ts
+++ projects/ng-oct-report/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/services/ng-oct-report.service';
+export * from './lib/services/category.service';
 export * from './lib/components/ng-oct-report.component';
 export * from './lib/ng-oct-report.module';
 export * from './lib/interfaces/ng-oct-report.interface'
